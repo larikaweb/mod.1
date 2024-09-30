6286e56ba4a4ff70e5d25e38db35bcb66ffe637b
larikaweb <mevsk82@gmail.com>
2024-09-30 16:27:43 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,15 @@
+name = "Катя"
+print(name)
+age = 41
+print(age)
+age = 41+1
+print(age)
+is_student = True
+print(is_student)
+
+# запросить прибавляемое новое
+test_text = input ("Введите число: ")
+# Преобразуем строку в целое число.
+test_number = int(test_text)
+# Выводим переменную
+print ("Age+новое будет: ", age+test_number)
